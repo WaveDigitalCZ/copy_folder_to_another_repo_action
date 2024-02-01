15f4add0b1a93a3bf473bbd4aba39d06328eae39
danstepanek <61269423+danstepanek@users.noreply.github.com>
2024-02-02 00:20:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ then
   INPUT_DESTINATION_BRANCH=master
 fi
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
-
+git config --global --add safe.directory /github/workspace
 INPUT_COMMIT_MSG="Update $(git show -s --format=%s)."
 
 CLONE_DIR=$(mktemp -d)
@@ -23,8 +23,6 @@ echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-
-git config --global --add safe.directory /github/workspace
 
 
 if [ -n "$INPUT_DESTINATION_BRANCH_CREATE" ]
