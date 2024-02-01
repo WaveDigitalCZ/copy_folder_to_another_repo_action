0d8f7661a7065461eb3118e238a123147cf63ac1
danstepanek <61269423+danstepanek@users.noreply.github.com>
2024-02-02 00:01:57 +0100

    chore: Commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,8 @@ echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-echo $(git show -s --format=%s)
+commitMessage = $(git show -s --format=%s)
+echo commitMessage
 
 if [ -n "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
